bf658dcece9880e63ed4ae1712e30ba2de5c8610
mayurjathan <mayurjathan999@gmail.com>
2025-07-06 22:17:32 +0530

    vercel deployment changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    // Ignores ESLint errors during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
